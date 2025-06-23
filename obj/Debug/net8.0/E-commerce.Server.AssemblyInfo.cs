8d0eecee54d601c27dd12b60c195b20ffefaeff6
Vishnu4-28 <vishnuyadav04282003@gmail.com>
2025-06-23 15:36:24 +0530

    test

--- obj/Debug/net8.0/E-commerce.Server.AssemblyInfo.cs
+++ obj/Debug/net8.0/E-commerce.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-commerce.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4c4eb63f79b893ac3e83e484ca1fc1c46354f3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2104244f9db3a05793b1d720401607d7d073cb37")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-commerce.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-commerce.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
